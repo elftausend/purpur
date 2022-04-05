3ed680936d0025665a5fac6357149df33d15b031
elftausend <skatsacc.gut24@gmail.com>
2022-04-05 07:09:52 +0200

    added csv read

---
+++ src/csv_load.rs
@@ -0,0 +1,85 @@
+use std::path::Path;
+
+use csv::Reader;
+
+use crate::shuffle;
+
+
+pub struct CSVReturn<T> {
+    pub x: Vec<T>,
+    pub y: Vec<T>,
+    pub max: T,
+    pub sample_count: usize,
+}
+
+impl <T>CSVReturn<T> {
+    pub fn sample_count(&self) -> usize {
+        self.sample_count
+    }
+
+    pub fn features(&self) -> usize {
+        self.x.len() / self.sample_count
+    }
+}
+
+pub struct CSVLoader {
+    shuffle: bool,
+}
+
+impl CSVLoader {
+    pub fn new(shuffle: bool) -> Self {
+        CSVLoader {
+            shuffle
+        }
+    }
+
+    pub fn load<T, P: AsRef<Path> >(&self, path: P) -> Result<CSVReturn<T>, std::io::Error> where T: Default + Copy + std::cmp::PartialOrd + std::str::FromStr {
+        let mut rdr = Reader::from_path(path).unwrap();
+        
+        let mut x: Vec<T> = Vec::new();
+        let mut y: Vec<T> = Vec::new();
+        let mut max = T::default();
+        let mut m: usize = 0;
+        
+        for result in rdr.records().into_iter() {
+            let mut count = 0;
+            let record = result.unwrap();
+            
+            for r in record.iter() {
+                let mut r = r.to_string();
+                r.retain(|c| c != '"');
+                
+                let value = match r.parse::<T>() {
+                    Ok(value) => value,
+                    Err(_) => panic!("Read value was not an integer."),
+                };
+
+                if count == 0 {
+                    y.push(value);
+                } else { 
+                    if value > max {
+                        max = value;
+                    }
+                    x.push(value)
+                }
+                count+=1;
+            }
+            m+=1;
+        }
+ 
+        if self.shuffle {
+            let shuffeled = shuffle(m, &x, &y);
+            x = shuffeled.0;
+            y = shuffeled.1;
+        }
+        let csv = CSVReturn {
+            y,
+            x,
+            max,
+            sample_count: m,
+            
+        };
+        Ok(csv)
+        
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod utils;
 mod transforms;
 mod image_return;
+mod csv_load;
 
 pub use utils::*;
 pub use transforms::*;
 pub use image_return::*;
+pub use csv_load::*;
